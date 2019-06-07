f7ddcf54f45b43710b9ec29d87b0916cd1ac072a
elle-young <missanyayoung@outlook.com>
2019-06-07 08:10:04 -0700

    update

---
+++ about.js
@@ -0,0 +1,25 @@
+// alert('Hello Human!');
+// var personsName = prompt('What is your name?');
+// console.log(personsName);
+//             alert('Hi, ' + personsName + ". We are going to ask some questions to see how well you know me.");
+            
+//             prompt('How many states have I lived in?'); //5
+
+
+//             console.log(personType);
+//             alert(personsName + ' ' + personType );
+//             var funThing = prompt('What do you do for fun?');
+//             console.log(funThing);
+//             alert(funThing + " sure is a fun activity!");
+// var planetName = prompt('What is your home planet?');
+// console.log(planetName);
+// alert('ok, ' + personsName + ' It sure was fun talking to you. Have fun with ' + funThing);
+//   alert('Goodbye!') 
+
+var GuessCorrect = [3, 5, 7, 9]
+var msg = "Guess the number I'm thinking of"
+var msg2 = "You're wrong!"
+var msg3 = "You're right!"
+var i = 1
+alert(Lets Play a Game)
+prompt("what is your name?")

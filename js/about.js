b9ae64d9ea829dbf253c11ceff423eba50bd39cb
elle-young <missanyayoung@outlook.com>
2019-06-12 16:18:54 -0700

    update with new console logs and new guessing game, 7 total questions

--- js/about.js
+++ js/about.js
@@ -14,6 +14,17 @@ function question1(userAnswer1){
   }
 }
 
+console.log('This was user input:' + userAnswer1);
+
+var userAnswer2 = prompt ('Have I lived in California?').charAt(0).toLowerCase(); //y
+function question2(userAnswer2){
+  if (userAnswer2==='y') {
+    alert('correct!');
+  } else (userAnswer2!=='y'); {
+    alert('try again!');
+  }
+}
+console.log('This was user input:' + userAnswer2);
 
 var userAnswer3 = prompt ('Was I born in Washington State?').charAt(0).toLowerCase(); //y
 function question3(userAnswer3){
@@ -23,27 +34,27 @@ function question3(userAnswer3){
     alert('try again!');
   }
 }
-console.log(userAnswer3);
+console.log('This was user input:' + userAnswer3);
 
-var userAnswer4 = prompt ('Do I like Burritos more than tacos?').charAt(0).toLowerCase(); //y
+var userAnswer4 = prompt ('Do I like tacos more than burritos?').charAt(0).toLowerCase(); //n
 function question4(userAnswer4){
-  if (userAnswer4==='y') {
+  if (userAnswer4==='n') {
     alert('correct!');
-  } else (userAnswer4!=='y'); {
+  } else (userAnswer4!=='n'); {
     alert('try again!');
   }
 }
-console.log (userAnswer4);
+console.log ('This was user input:' + userAnswer4);
 
-var userAnswer5 = prompt ('Have you had fun?').charAt(0).toLowerCase();
+var userAnswer5 = prompt ('Are you having fun?').charAt(0).toLowerCase();
 function question5(userAnswer5){
   if (userAnswer5==='y') {
-    alert('correct!');
+    alert('I\'m glad!');
   } else (userAnswer5!=='y'); {
-    alert('try again!');
+    alert('Sad!');
   }
 }
-console.log (userAnswer5);
+console.log ('This was user input:' + userAnswer5);
 
 var userTries = 4;
 
@@ -60,10 +71,27 @@ function question6(userAnswer6){
   }
   userTries--;
 }
-console.log (userAnswer6);
+console.log ('This was user input:' + userAnswer6);
+
+var userAnswer7 = prompt ('What number am I thinking of?').charAt(0).toLowerCase(); //66
+function question7(userAnswer7){
+  while (userTries < 6 && userAnswer7 < 66) {
+    alert('sorry, too low. Try again');
+  }
+  if (userAnswer7 > 66) {
+    alert('sorry, too high');
+  }
+  if (userAnswer7 === 66); {
+    alert('Congrats, you win!');
+  }
+  userTries--;
+}
+console.log ('This was user input:' + userAnswer7);
 
 question1(userAnswer1);
+question2(userAnswer2);
 question3(userAnswer3);
 question4(userAnswer4);
 question5(userAnswer5);
 question6(userAnswer6);
+question7(userAnswer7);

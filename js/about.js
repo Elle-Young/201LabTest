5c0ae064c0ba1d7d9809d268c1434f66c3058c61
elle-young <missanyayoung@outlook.com>
2019-06-23 19:06:07 -0700

    working on bug loop

--- js/about.js
+++ js/about.js
@@ -1,4 +1,5 @@
 'use strict';
+console.log('Game has infinite loop, so it is currently under construction.');
 
 var userAnswer1 = prompt ('Do you want to play a game?').charAt(0).toLowerCase();
 function question1(userAnswer1){
@@ -26,72 +27,72 @@ function question2(userAnswer2){
 }
 console.log('This was user input:' + userAnswer2);
 
-var userAnswer3 = prompt ('Was I born in Washington State?').charAt(0).toLowerCase(); //y
-function question3(userAnswer3){
-  if (userAnswer3==='y') {
-    alert('correct!');
-  } else (userAnswer3!=='y'); {
-    alert('try again!');
-  }
-}
-console.log('This was user input:' + userAnswer3);
+// var userAnswer3 = prompt ('Was I born in Washington State?').charAt(0).toLowerCase(); //y
+// function question3(userAnswer3){
+//   if (userAnswer3==='y') {
+//     alert('correct!');
+//   } else (userAnswer3!=='y'); {
+//     // alert('try again!');
+//   }
+// }
+// console.log('This was user input:' + userAnswer3);
 
-var userAnswer4 = prompt ('Do I like tacos more than burritos?').charAt(0).toLowerCase(); //n
-function question4(userAnswer4){
-  if (userAnswer4==='n') {
-    alert('correct!');
-  } else (userAnswer4!=='n'); {
-    alert('try again!');
-  }
-}
-console.log ('This was user input:' + userAnswer4);
+// var userAnswer4 = prompt ('Do I like tacos more than burritos?').charAt(0).toLowerCase(); //n
+// function question4(userAnswer4){
+//   if (userAnswer4==='n') {
+//     alert('correct!');
+//   } else (userAnswer4!=='n'); {
+//     // alert('try again!');
+//   }
+// }
+// console.log ('This was user input:' + userAnswer4);
 
-var userAnswer5 = prompt ('Are you having fun?').charAt(0).toLowerCase();
-function question5(userAnswer5){
-  if (userAnswer5==='y') {
-    alert('I\'m glad!');
-  } else (userAnswer5!=='y'); {
-    alert('Sad!');
-  }
-}
-console.log ('This was user input:' + userAnswer5);
+// var userAnswer5 = prompt ('Are you having fun?').charAt(0).toLowerCase();
+// function question5(userAnswer5){
+//   if (userAnswer5==='y') {
+//     alert('I\'m glad!');
+//   } else (userAnswer5!=='y'); {
+//     alert('Sad!');
+//   }
+// }
+// console.log ('This was user input:' + userAnswer5);
 
-var userTries = 4;
+// var userTries = 4;
 
-var userAnswer6 = prompt ('What is the square root of 144?').charAt(0).toLowerCase(); //12
-function question6(userAnswer6){
-  while (userTries < 4 && userAnswer6 < 12) {
-    alert('sorry, too low. Try again');
-  }
-  if (userAnswer6 > 12) {
-    alert('sorry, too high');
-  }
-  if (userAnswer6 === 12); {
-    alert('Congrats, you win!');
-  }
-  userTries--;
-}
-console.log ('This was user input:' + userAnswer6);
+// var userAnswer6 = prompt ('What is the square root of 144?').charAt(0).toLowerCase(); //12
+// function question6(userAnswer6){
+//   while (userTries < 4 && userAnswer6 < 12) {
+//     alert('sorry, too low. Try again');
+//   }
+//   if (userAnswer6 > 12) {
+//     alert('sorry, too high');
+//   }
+//   if (userAnswer6 === 12); {
+//     alert('Congrats, you win!');
+//   }
+//   userTries--;
+// }
+// console.log ('This was user input:' + userAnswer6);
 
-var userAnswer7 = prompt ('What number am I thinking of?').charAt(0).toLowerCase(); //66
-function question7(userAnswer7){
-  while (userTries < 6 && userAnswer7 < 66) {
-    alert('sorry, too low. Try again');
-  }
-  if (userAnswer7 > 66) {
-    alert('sorry, too high');
-  }
-  if (userAnswer7 === 66); {
-    alert('Congrats, you win!');
-  }
-  userTries--;
-}
-console.log ('This was user input:' + userAnswer7);
-
+// var userAnswer7 = prompt ('What number am I thinking of?').charAt(0).toLowerCase(); //66
+// function question7(userAnswer7){
+//   while (userTries < 6 && userAnswer7 < 66) {
+//     alert('sorry, too low. Try again');
+//   }
+//   if (userAnswer7 > 66) {
+//     alert('sorry, too high');
+//   }
+//   if (userAnswer7 === 66); {
+//     alert('Congrats, you win!');
+//   }
+//   userTries--;
+// }
+// console.log ('This was user input:' + userAnswer7);
+// you lose
 question1(userAnswer1);
 question2(userAnswer2);
-question3(userAnswer3);
-question4(userAnswer4);
-question5(userAnswer5);
-question6(userAnswer6);
-question7(userAnswer7);
+// question3(userAnswer3);
+// question4(userAnswer4);
+// question5(userAnswer5);
+// question6(userAnswer6);
+// question7(userAnswer7);
